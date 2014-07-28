d4ab6d34f0294b57afa453aad96ddcad78b0eef5
Kill Face <killface@TheAnnihilatrix.(none)>
2014-07-28 18:19:00 -0400

    added new models

--- musiclibrary/albums/models.py
+++ musiclibrary/albums/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=120)
+    website = models.URLField(null=True)
+    mbid = models.CharField(max_length=40)
+
+    def __unicode__(self):
+        return self.name
+
+class Label(models.Model):
+    name = models.CharField(max_length=120)
+    website = models.URLField(null=True)
+    mbid = models.CharField(max_length=40)
+
+    def __unicode__(self):
+        return self.name
+
+class Album(models.Model):
+    title = models.CharField(max_length=120)
+    artist = models.ForeignKey(Artist)
+    label = models.ForeignKey(Label)
+    mbid = models.CharField(max_length=40)
+
+    def __unicode__(self):
+        return "%s, by %s" % (self.title, self.artist)
+
+
