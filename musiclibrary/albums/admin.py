004325006185e42874ab3d8fb1dbc509eccc8ae7
Kill Face <killface@TheAnnihilatrix.(none)>
2014-07-29 21:16:53 -0400

    apps added

--- musiclibrary/albums/admin.py
+++ musiclibrary/albums/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from albums.models import Album
+from albums.models import Album, Artist, Label
 
 admin.site.register(Album)
+admin.site.register(Artist)
+admin.site.register(Label)
